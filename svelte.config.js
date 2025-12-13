7b9f934e49c3dd5e3add82e15888f41bc607427a
Mike Grunwald <125307636+mikegrunwald@users.noreply.github.com>
2025-12-13 13:17:18 -0600

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,17 +4,7 @@ import adapter from "@sveltejs/adapter-cloudflare";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: "200.html", // allows client-side routing on Cloudflare Pages
-      strict: true
-    }),
-
-    prerender: {
-      entries: ["*"], // ensures the entire site is pre-rendered
-      handleHttpError: "ignore" // prevents failure on 404 pages
-    },
+    adapter: adapter(),
 
     alias: {
       $assets: "src/assets",
